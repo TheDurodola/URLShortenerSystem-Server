b97d6ea3a6e00a4f3990a9cfd2e860b1efe9e2ea
Durodola Abolaji <142089766+TheDurodola@users.noreply.github.com>
2025-09-10 01:52:08 +0100

    initial commit

---
+++ src/main/java/com/yrsd/url_shortener/UrlShortenerSystemApplication.java
@@ -0,0 +1,13 @@
+package com.yrsd.url_shortener;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UrlShortenerSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UrlShortenerSystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/yrsd/url_shortener/UrlShortenerSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yrsd.url_shortener;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UrlShortenerSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
